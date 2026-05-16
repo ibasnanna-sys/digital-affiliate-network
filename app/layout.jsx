600388917301b2feaa708e0e038003dc0c1939d7
ibasnanna-sys <ibas.nanna@gmail.com>
2026-05-16 17:37:06 +0800

    Add RootLayout component with metadata

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Digital Affiliate Network',
+  description: 'Platform layanan digital modern',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
